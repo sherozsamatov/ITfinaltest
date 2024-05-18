f11ce1a125b433c87b575509ad1f46e276c656eb
Sheroz Samatov <sherozonline@gmail.com>
2024-05-18 10:43:20 +0100

    Создаем новый временный массив для хранения элементов, длина которых меньше или равна 3 символам

--- Program.cs
+++ Program.cs
@@ -6,6 +6,24 @@ class Program
   {
     // Задаем начальный массив строк
     string[] initialArray = { "Hello", "2", "world", ":-)" };
+
+    // Вызываем метод для формирования нового массива
+    string[] newArray = GetNewArray(initialArray);
+
+    // Выводим метод для формирования нового массива
+    Console.WriteLine("Новый массив:");
+    foreach (string str in newArray)
+    {
+      Console.Write(str + " ");
+    }
+    Console.ReadLine();
+
+  }
+  static string[] GetNewArray(string[] inputArray)
+  {
+    // Создаем новый временный массив для хранения элементов, длина которых меньше или равна 3 символам
+    string[] tempArray = new string[inputArray.Length];
+    int count = 0;
     
   }
 }
