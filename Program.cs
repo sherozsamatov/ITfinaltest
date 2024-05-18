76a3006aad0ff5512585673ee76df664d50276cd
Sheroz Samatov <sherozonline@gmail.com>
2024-05-18 10:32:01 +0100

    задаем начальный массив

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+  static void Main(Strings[] args)
+  {
+    // Задаем начальный массив строк
+    string[] initialArray = { "Hello", "2", "world", ":-)" };
+    
+  }
+}
