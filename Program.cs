86cfab095879d16910bc3a5c7ec53fdadf868da4
Sheroz Samatov <sherozonline@gmail.com>
2024-05-18 13:02:09 +0100

    создаем новый массив с нужной длиной

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-class Program
+﻿class Program
 {
   static void Main(Strings[] args)
   {
@@ -24,6 +22,23 @@ class Program
     // Создаем новый временный массив для хранения элементов, длина которых меньше или равна 3 символам
     string[] tempArray = new string[inputArray.Length];
     int count = 0;
-    
+
+    // Проходим по каждому элементу исходного массива
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+      // Если длина элемента массива меньше или равна 3 символам, добавляем его во временный массив
+      if (inputArray[i].Length <= 3)
+      {
+        tempArray[count] = inputArray[i];
+        count++;
+      }
+
+    }
+    // Создаем новый массив с нужной длиной
+    string[] resultArray = new string[count];
+    Array.Copy(tempArray, resultArray, count);
+
+    return resultArray;
+
   }
 }
